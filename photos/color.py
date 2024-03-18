4db352faea0ae0ef2ad018aea517325cc8ae6c38
zyh <250187310@qq.com>
2024-03-18 20:18:30 +0800

    沿红色路线跑的自动驾驶

---
+++ photos/color.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+
+    Img = cv2.imread('red.jpg')  # 读入一幅图像
+
+    kernel_2 = np.ones((2, 2), np.uint8)  # 2x2的卷积核
+    kernel_3 = np.ones((3, 3), np.uint8)  # 3x3的卷积核
+    kernel_4 = np.ones((4, 4), np.uint8)  # 4x4的卷积核
+
+    print(kernel_2)
+    print(kernel_3)
+    print(kernel_4)
+    if Img is not None:  # 判断图片是否读入
+        HSV = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)  # 把BGR图像转换为HSV格式
+
+        # 下面两个值是要识别的颜色范围
+        Lower = np.array([0, 43, 46])  # 要识别颜色的下限
+        Upper = np.array([156, 180, 255])  # 要识别的颜色的上限
+
+        # mask是把HSV图片中在颜色范围内的区域变成白色，其他区域变成黑色
+        mask = cv2.inRange(HSV, Lower, Upper)
+
+        # 下面四行是用卷积进行滤波
+        # erode()函数可以对输入图像用特定结构元素进行腐蚀操作，该结构元素确定腐蚀操作过程中的邻域的形状，
+        # 各点像素值将被替换为对应邻域上的最小值：
+        erosion = cv2.erode(mask, kernel_3, iterations=1)
+        erosion = cv2.erode(erosion, kernel_3, iterations=1)
+        # dilate()函数可以对输入图像用特定结构元素进行膨胀操作，该结构元素确定膨胀操作过程中的邻域的形状，
+        # 各点像素值将被替换为对应邻域上的最大值：
+        dilation = cv2.dilate(erosion, kernel_3, iterations=1)
+        dilation = cv2.dilate(dilation, kernel_3, iterations=1)
+
+        # target是把原图中的非目标颜色区域去掉剩下的图像
+        target = cv2.bitwise_and(Img, Img, mask=dilation)
+
+        # 将滤波后的图像变成二值图像放在binary中
+        ret, binary = cv2.threshold(dilation, 127, 255, cv2.THRESH_BINARY)
+
+        # 在binary中发现轮廓，轮廓按照面积从小到大排列
+        contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        p = 0
+        for i in contours:  # 遍历所有的轮廓
+            x, y, w, h = cv2.boundingRect(i)  # 将轮廓分解为识别对象的左上角坐标和宽、高
+            # 在图像上画上矩形（图片、左上角坐标、右下角坐标、颜色、线条宽度）
+            cv2.rectangle(Img, (x, y), (x + w, y + h), (0, 255,), 3)
+            # 给识别对象写上标号
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(Img, str(p), (x - 10, y + 10), font, 1, (0, 0, 255), 2)  # 加减10是调整字符位置
+            p += 1
+
+        print('黄色方块的数量是', p, '个')  # 终端输出目标数量
+
+        # cv2.imshow('target', target)
+        # cv2.imshow('Mask', mask)
+        # cv2.imshow("prod", dilation)
+        cv2.imshow('Img', binary)
+        cv2.imwrite('Img.png', Img)  # 将画上矩形的图形保存到当前目录
+
+    while True:
+        Key = chr(cv2.waitKey(15) & 255)
+        if Key == 'q':
+            cv2.destroyAllWindows()
+            break
--- road_detect.py
+++ road_detect.py
@@ -2,19 +2,21 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 import numpy as np
 
-img0 = cv.imread('./photos/y.png')
+img0 = cv.imread('./photos/red.jpg')
 #img0 = img0[0:300,600:1000] #这里截取摄像头拍摄到的路面部分 因为摄像头相对位置固定 所以只需要调整一次
 def color_detect(img):
     # 读取图像并转换到HSV空间
     frame = img
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-# 黄色线检测
-    lower_blue = np.array([11, 43, 46])
-    upper_blue = np.array([34, 255, 255])
-    yellow_mask = cv.inRange(hsv, lower_blue, upper_blue)
-    cv.imwrite('./photos/yellow_mask.jpg', yellow_mask)
-    img_show(yellow_mask,"yellow")
+# 红色线检测
+    lower_red = np.array([0, 70, 72])
+    upper_red = np.array([7, 255, 255])
+    red_mask = cv.inRange(hsv, lower_red, upper_red)
+    #red_mask = cv.bitwise_and(frame, frame, mask=red_mask)
+    red_mask=cv.bitwise_not(red_mask)
+    cv.imwrite('./photos/red_mask.jpg', red_mask)
+    img_show(red_mask,"red")
 # 白色线检测
     lower_blue = np.array([0, 0, 200])
     upper_blue = np.array([180, 30, 255])
@@ -24,6 +26,12 @@ def find_edge(img):
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (5, 5), 0)
     ret, img1 = cv.threshold(blur, 180, 255, cv.THRESH_BINARY)  # 二值化
+    img_show(img1,'1')
+    histogram = np.sum(img1[img1.shape[0] // 2:, :], axis=0)  #将img1读取行数 取中间行 取全部列 之后压缩为一行 即所有列相加
+    print(histogram)
+    midpoint = int(histogram.shape[0] / 2)  # 直方图垂直尺寸
+    edged = cv.erode(img1, None, iterations=1)
+    img_show(img1,'s1s')
     edged = cv.Canny(img1,80, 100)
     contours, h = cv.findContours(edged.copy(), mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)   #只储存车道的顶点数据 只检测外轮廓
     largest_contours = sorted(contours, key=cv.contourArea, reverse=True)[0:1]   #找到面积最大的车道
@@ -42,19 +50,19 @@ def find_edge(img):
         for i in cnt:
           lg_x.append(i[0][0])
           lg_y.append(i[0][1])
-          print(lg_x[numlg],',',lg_y[numlg])
+          #print(lg_x[numlg],',',lg_y[numlg])
           #print(numlg)
           numlg+=1
-          print(numlg)
+          #print(numlg)
     for cnt in sec_largest_contours:   #次大车道的边缘坐标
 
       for i in cnt:
          se_x.append(i[0][0])
          se_y.append(i[0][1])
-         print(se_x[numse],',',se_y[numse])
+         #print(se_x[numse],',',se_y[numse])
          #print(numlg)
          numse+=1
-         print(numse)
+         #print(numse)
     for cnt in range(int(min(numlg,numse)/2)):    #绘制路线
         cv.circle(driving_reference, (int((lg_x[cnt]+se_x[cnt])/2),lg_y[cnt]), 1, (0, 0, 213), 3) #方法是取x平均值 以最大车道的Y坐标为路线的Y坐标 Y坐标取值可能有问题
         #cv.circle(driving_reference, (604,387), 3, (0, 0, 213), 3)
@@ -69,11 +77,11 @@ def find_edge(img):
     #cv.namedWindow('Image', 0)
 def img_show(img,name):
     cv.imshow(name, img)
-yellow = cv.imread('./photos/y.png')
+yellow = cv.imread('./photos/aaa.jpg')
 color_detect(yellow)
-yellow_edge=cv.imread('./photos/yellow_mask.jpg')
-img_show(yellow_edge,'yellow')
-find_edge(yellow_edge)
+#yellow_edge=cv.imread('./photos/yellow_mask.jpg')
+#img_show(yellow_edge,'yellow')
+#find_edge(yellow_edge)
 
 #cv.imshow("orgin", img0)
 #cv.imshow("Image", img1)
